eb40486fd81341a76da5d3deb78eb7e04bd50411
Sean Young <seanism@gmail.com>
2021-08-10 14:27:13 -0700

    Default offboarding
    Added the option to default to transfer files and forward email to manager and wipe calendar.

--- deprovision.sh
+++ deprovision.sh
@@ -3,6 +3,7 @@
 
 gam="$HOME/bin/gam/gam"
 
+echo ""
 # Enter user to deprovision
 read  -p "Enter the email address you wish to deprovision: " username
 
@@ -13,33 +14,42 @@ if [[ $response =~ [nN] ]]
     read -p "Please deactivate the user within Okta then return to this script.  Once completed press return to continue."
 fi
 
-# Confirm user name before deprovisioning
-read -r -p "Ready to deprovision $username? [y/n]: " response
-if [[ $response =~ [nN] ]]
-  then
-		echo "Exiting"
-		exit
-fi
-
+echo ""
 # Verifying account is not suspended
 echo "-> Unsuspending user account"
 $gam update user $username suspended off
 
-# Should user's calendar be wiped? If so, it will be wiped later
-read -p "Do you want to Wipe $username's calendar? [y/n]: " cal_response
+echo ""
 
-# Email forwarding?
-read -p "Did you want their email forwarded? [y/n]: " emailforward
-if [[ $emailforward =~ [yY] ]]
-then
-  read -p "What account (email address) should email be forwarded to? " emailforward
+# Employee offboard standard setup
+read -r -p "Did you want to do the standard offboard? (forward email & transfer files to manager) [y/n]: " response
+if [[ $response =~ [yY] ]]
+  then
+		manageremail=$($gam info user $username | grep 'value: ' | tail -1 | sed 's/value: //')
+    cal_response="y"
+    emailforward="y"
+    emailforwardaddress=$manageremail
+    drivetransfer=$manageremail
+    ooo="n"
+	else
+    # Should user's calendar be wiped? If so, it will be wiped later
+    read -p "Do you want to wipe $username's calendar? [y/n]: " cal_response
+
+    # Email forwarding?
+    read -p "Did you want their email forwarded? [y/n]: " emailforward
+    if [[ $emailforward =~ [yY] ]]
+    then
+      read -p "What account (email address) should email be forwarded to? " emailforwardaddress
+    fi
+
+    # Transfer docs to another employee
+    read -p "Email address to receive Google Drive files: " drivetransfer
+
+    # Set OOO
+    read -p "Should there be an out of office set? [y/n]: " ooo
 fi
 
-# Transfer docs to another employee
-read -p "Email address to receive Google Drive files: " drivetransfer
 
-# Set OOO
-read -p "Should there be an out of office set? [y/n]: " ooo
 
 # Starting deprovision
 echo ""
@@ -83,22 +93,12 @@ echo "-> Generating new 2FA Recovery Codes for $username"
 $gam user $username update backupcodes | tee -a /Users/Shared/$username.log
 } &> /dev/null
 
-# Removing all of user's calendar events if previously selected
-if [[ $cal_response =~ [yY] ]]
-then
-		echo "Deleting all of "$username"'s calendar events"
-		$gam calendar $username wipe | tee -a /Users/Shared/$username.log
-else
-		echo "Not wiping calendar" | tee -a /Users/Shared/$username.log
-fi
-
-
 ## Forwarding email
 if [[ $emailforward =~ [yY] ]]
 then
 		echo "Forwarding "$username"'s email"
-		$gam user $username add forwardingaddress $emailforward | tee -a /Users/Shared/$username.log
-    $gam user $username forward on $emailforward markread
+		$gam user $username add forwardingaddress $emailforwardaddress | tee -a /Users/Shared/$username.log
+    $gam user $username forward on $emailforwardaddress markread
 else
 		echo "Not forwarding email" | tee -a /Users/Shared/$username.log
 fi
@@ -118,6 +118,17 @@ fi
 echo "-> Transfering Google Drive to $drivetransfer"
 $gam create datatransfer $username gdrive $drivetransfer privacy_level shared,private
 echo "Drive transfer initiated" | tee -a /Users/Shared/$username.log
+
+# Removing all of user's calendar events if previously selected
+if [[ $cal_response =~ [yY] ]]
+then
+    echo "Waiting 10 seconds for suspension to be removed..."
+    sleep 10
+		echo "Deleting all of "$username"'s calendar events"
+		$gam calendar $username wipe | tee -a /Users/Shared/$username.log
+else
+		echo "Not wiping calendar" | tee -a /Users/Shared/$username.log
+fi
 
 month=$(LANG=en_us_88591; date "+%B");
 
